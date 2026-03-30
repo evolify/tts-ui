82fcfc27adaf81092ea4df7191f52a4c1d2ba374
evolify <wolf@evolify.cn>
2026-03-30 18:18:14 +0800

    feat: add gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import { GoogleTagManager } from "@next/third-parties/google"
 import "./globals.css"
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <GoogleTagManager gtmId="GTM-MPB83PS6" />
     </html>
   )
 }
